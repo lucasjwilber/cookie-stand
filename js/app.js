57ac9f4746948f750d766a9926913a1207522b28
Lucas Wilber <lucasjwilber@gmail.com>
2019-09-17 15:25:53 -0700

    finally fixed the js after about 1.5 hours of troubleshooting. then quickly fixed the table generator

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 'use strict'
 
 //this is where the all the lists for each shop are created on sales.html
-var listsArea = document.getElementById('salesUl');
 var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
-var stores = [];
 
 //store constructor function
 function Store(location, minCustomersPerHour, maxCustomersPerHour, avgCookiesPerCustomer) {
@@ -17,46 +15,91 @@ function Store(location, minCustomersPerHour, maxCustomersPerHour, avgCookiesPer
   Store.stores.push(this);
 }
 
-//master stores list is a prototype
 Store.stores = [];
 
-//dayCalc prototype:
+
 Store.prototype.dayCalc = function () {
   //1. for every hour of operation, 
   for (var i = 0; i < hoursOfOperation.length; i++) {
     //calculate a random number of customers... 
     var customersPerHour = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
-
-    //...and then using that a number of cookies sold
+    //...and then using that, a number of cookies sold
     var cookiesSoldPerHour = Math.round(customersPerHour * this.avgCookiesPerCustomer);
-
-    //then push the data to the dailySales array
-    this.dailySales.push(`${hoursOfOperation[i]}: ${cookiesSoldPerHour} cookies`);
+    //then push that data to the dailySales array
+    this.dailySales.push(cookiesSoldPerHour);
 
     //update the running total of cookies sold
     this.dailyTotal += cookiesSoldPerHour;
   }
 
   //then add the total to the end of the dailySales array
-  this.dailySales.push(`Total: ${this.dailyTotal} cookies`);
+  this.dailySales.push(this.dailyTotal);
 }
 
 
-Store.prototype.siteUpdate = function () {
-  //create a ul on sales.html for this store and name it
-  var salesList = document.createElement('ul');
-  salesList.textContent = `${this.location}`;
 
-  //for each hour of operation, create an li using info from the dailySales array, and append it to the salesList ul
-  for (var i = 0; i < this.dailySales.length; i++) {
-    var li = document.createElement('li');
-    li.textContent = this.dailySales[i];
-    salesList.appendChild(li);
+
+function updateTable() {
+
+  //first run dayCalc for each store to populate the sales array
+  for (var i = 0; i < Store.stores.length; i++) {
+    Store.stores[i].dayCalc();
   }
 
-  //then append the whole list to the site's master list
-  listsArea.appendChild(salesList);
-};
+
+  var salesTable = document.getElementById('salesData');
+
+  //TABLE HEADER:
+  //create a table header and a row for it, append them
+  var tableHeader = document.createElement('thead');
+  salesTable.appendChild(tableHeader);
+  var tableHeaderRow = document.createElement('tr');
+  tableHeader.appendChild(tableHeaderRow);
+
+  //add a blank td for the top left area
+  var topLeftBlock = document.createElement('td');
+  tableHeaderRow.appendChild(topLeftBlock);
+
+  //add a td for each hour of the day
+  for (var i = 0; i < hoursOfOperation.length; i++) {
+    var hourTd = document.createElement('td');
+    hourTd.textContent = hoursOfOperation[i];
+    tableHeaderRow.appendChild(hourTd);
+  }
+
+  //add a Total td at the end
+  var totalTd = document.createElement('td');
+  totalTd.textContent = 'Total';
+  tableHeaderRow.appendChild(totalTd);
+
+
+  ///////////////////TABLE BODY:
+  var tbody = document.createElement('tbody');
+  salesTable.appendChild(tbody);
+
+  //for each store in Store.stores,
+  for (var i = 0; i < Store.stores.length; i++) {
+
+    //make a row for this store and append it to tbody
+    var storeRow = document.createElement('tr');
+    tbody.appendChild(storeRow);
+
+    //make the first td the store name
+    var storeName = document.createElement('td');
+    storeName.textContent = Store.stores[i].location;
+    storeRow.appendChild(storeName);
+    
+    // make a td for each hour and append it to the trow
+    for (var j = 0; j < Store.stores[i].dailySales.length; j++) {
+      var hourData = document.createElement('td');
+      hourData.textContent = Store.stores[i].dailySales[j];
+      storeRow.appendChild(hourData);
+    }
+  }
+
+}
+
+
 
 var seattle = new Store('Seattle', 23, 65, 6.3);
 var tokyo = new Store('Tokyo', 3, 24, 1.2);
@@ -65,11 +108,5 @@ var paris = new Store('Paris', 20, 38, 2.3);
 var lima = new Store('Lima', 2, 16, 4.6);
 
 
-function updateAll() {
-  for (var i = 0; i < Store.stores.length; i++) {
-    Store.stores[i].dayCalc();
-    Store.stores[i].siteUpdate();
-  }
-}
+updateTable();
 
-updateAll();

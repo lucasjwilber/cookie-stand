d66b929bb91c028535e48f0a62027da33defc0e0
Lucas Wilber <lucasjwilber@gmail.com>
2019-09-17 12:10:26 -0700

    committing what I added while learning about constructors during the lecture

--- js/app.js
+++ js/app.js
@@ -2,258 +2,74 @@
 
 //this is where the all the lists for each shop are created on sales.html
 var listsArea = document.getElementById('salesUl');
+var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
+var stores = [];
 
+//store constructor function
+function Store(location, minCustomersPerHour, maxCustomersPerHour, avgCookiesPerCustomer) {
+  this.location = location;
+  this.minCustomersPerHour = minCustomersPerHour;
+  this.maxCustomersPerHour = maxCustomersPerHour;
+  this.avgCookiesPerCustomer = avgCookiesPerCustomer;
+  this.dailySales = [];
+  this.dailyTotal = 0;
+  //add this store to the master stores list array
+  Store.stores.push(this);
+}
 
-var seattle = {
-  location: 'Seattle',
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',],
-  minCustomersPerHour: 23,
-  maxCustomersPerHour: 65,
-  avgCookiesPerCustomer: 6.3,
-  dailySales: [],
-  dailyTotal: 0,
-  dayCalc() {
-    //1. for every hour of operation, 
-    for (var i = 0; i < this.hoursOfOperation.length; i++) {
+//master stores list is a prototype
+Store.stores = [];
 
-      //calculate a random number of customers... 
-      var customersPerHour = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour) + this.minCustomersPerHour);
+//dayCalc prototype:
+Store.prototype.dayCalc = function () {
+  //1. for every hour of operation, 
+  for (var i = 0; i < hoursOfOperation.length; i++) {
+    //calculate a random number of customers... 
+    var customersPerHour = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
 
-      //...and then using that a number of cookies sold
-      var cookiesSoldPerHour = Math.round(customersPerHour * this.avgCookiesPerCustomer);
+    //...and then using that a number of cookies sold
+    var cookiesSoldPerHour = Math.round(customersPerHour * this.avgCookiesPerCustomer);
 
-      //then push the data to the dailySales array
-      this.dailySales.push(`${this.hoursOfOperation[i]}: ${cookiesSoldPerHour} cookies`);
+    //then push the data to the dailySales array
+    this.dailySales.push(`${hoursOfOperation[i]}: ${cookiesSoldPerHour} cookies`);
 
-      //update the running total of cookies sold
-      this.dailyTotal += cookiesSoldPerHour;
-    }
-
-    //then add the total to the end of the dailySales array
-    this.dailySales.push(`Total: ${this.dailyTotal} cookies`);
-  },
-  siteUpdate() {
-
-    //create a ul on sales.html for this store and name it
-    var salesList = document.createElement('ul');
-    salesList.textContent = `${this.location}`;
-
-    //for each hour of operation, create an li using info from the dailySales array, and append it to the salesList ul
-    for (var i = 0; i < this.dailySales.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = this.dailySales[i];
-      salesList.appendChild(li);
-    }
-
-    //then append the whole list to the site's master list
-    listsArea.appendChild(salesList);
+    //update the running total of cookies sold
+    this.dailyTotal += cookiesSoldPerHour;
   }
+
+  //then add the total to the end of the dailySales array
+  this.dailySales.push(`Total: ${this.dailyTotal} cookies`);
 }
 
 
+Store.prototype.siteUpdate = function () {
+  //create a ul on sales.html for this store and name it
+  var salesList = document.createElement('ul');
+  salesList.textContent = `${this.location}`;
 
-var tokyo = {
-  location: 'Tokyo',
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',],
-  minCustomersPerHour: 3,
-  maxCustomersPerHour: 24,
-  avgCookiesPerCustomer: 1.2,
-  dailySales: [],
-  dailyTotal: 0,
-  dayCalc() {
-    //1. for every hour of operation, 
-    for (var i = 0; i < this.hoursOfOperation.length; i++) {
-
-      //calculate a random number of customers... 
-      var customersPerHour = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour) + this.minCustomersPerHour);
-
-      //...and then using that a number of cookies sold
-      var cookiesSoldPerHour = Math.round(customersPerHour * this.avgCookiesPerCustomer);
-
-      //then push the data to the dailySales array
-      this.dailySales.push(`${this.hoursOfOperation[i]}: ${cookiesSoldPerHour} cookies`);
-
-      //update the running total of cookies sold
-      this.dailyTotal += cookiesSoldPerHour;
-    }
-
-    //then add the total to the end of the dailySales array
-    this.dailySales.push(`Total: ${this.dailyTotal} cookies`);
-  },
-  siteUpdate() {
-
-    //create a ul on sales.html for this store and name it
-    var salesList = document.createElement('ul');
-    salesList.textContent = `${this.location}`;
-
-    //for each hour of operation, create an li using info from the dailySales array, and append it to the salesList ul
-    for (var i = 0; i < this.dailySales.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = this.dailySales[i];
-      salesList.appendChild(li);
-    }
-
-    //then append the whole list to the site's master list
-    listsArea.appendChild(salesList);
+  //for each hour of operation, create an li using info from the dailySales array, and append it to the salesList ul
+  for (var i = 0; i < this.dailySales.length; i++) {
+    var li = document.createElement('li');
+    li.textContent = this.dailySales[i];
+    salesList.appendChild(li);
   }
-}
 
+  //then append the whole list to the site's master list
+  listsArea.appendChild(salesList);
+};
 
-
-var dubai = {
-  location: 'Dubai',
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',],
-  minCustomersPerHour: 11,
-  maxCustomersPerHour: 38,
-  avgCookiesPerCustomer: 3.7,
-  dailySales: [],
-  dailyTotal: 0,
-  dayCalc() {
-    //1. for every hour of operation, 
-    for (var i = 0; i < this.hoursOfOperation.length; i++) {
-
-      //calculate a random number of customers... 
-      var customersPerHour = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour) + this.minCustomersPerHour);
-
-      //...and then using that a number of cookies sold
-      var cookiesSoldPerHour = Math.round(customersPerHour * this.avgCookiesPerCustomer);
-
-      //then push the data to the dailySales array
-      this.dailySales.push(`${this.hoursOfOperation[i]}: ${cookiesSoldPerHour} cookies`);
-
-      //update the running total of cookies sold
-      this.dailyTotal += cookiesSoldPerHour;
-    }
-
-    //then add the total to the end of the dailySales array
-    this.dailySales.push(`Total: ${this.dailyTotal} cookies`);
-  },
-  siteUpdate() {
-
-    //create a ul on sales.html for this store and name it
-    var salesList = document.createElement('ul');
-    salesList.textContent = `${this.location}`;
-
-    //for each hour of operation, create an li using info from the dailySales array, and append it to the salesList ul
-    for (var i = 0; i < this.dailySales.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = this.dailySales[i];
-      salesList.appendChild(li);
-    }
-
-    //then append the whole list to the site's master list
-    listsArea.appendChild(salesList);
-  }
-}
-
-
-
-var paris = {
-  location: 'Paris',
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',],
-  minCustomersPerHour: 20,
-  maxCustomersPerHour: 38,
-  avgCookiesPerCustomer: 2.3,
-  dailySales: [],
-  dailyTotal: 0,
-  dayCalc() {
-    //1. for every hour of operation, 
-    for (var i = 0; i < this.hoursOfOperation.length; i++) {
-
-      //calculate a random number of customers... 
-      var customersPerHour = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour) + this.minCustomersPerHour);
-
-      //...and then using that a number of cookies sold
-      var cookiesSoldPerHour = Math.round(customersPerHour * this.avgCookiesPerCustomer);
-
-      //then push the data to the dailySales array
-      this.dailySales.push(`${this.hoursOfOperation[i]}: ${cookiesSoldPerHour} cookies`);
-
-      //update the running total of cookies sold
-      this.dailyTotal += cookiesSoldPerHour;
-    }
-
-    //then add the total to the end of the dailySales array
-    this.dailySales.push(`Total: ${this.dailyTotal} cookies`);
-  },
-  siteUpdate() {
-
-    //create a ul on sales.html for this store and name it
-    var salesList = document.createElement('ul');
-    salesList.textContent = `${this.location}`;
-
-    //for each hour of operation, create an li using info from the dailySales array, and append it to the salesList ul
-    for (var i = 0; i < this.dailySales.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = this.dailySales[i];
-      salesList.appendChild(li);
-    }
-
-    //then append the whole list to the site's master list
-    listsArea.appendChild(salesList);
-  }
-}
-
-
-
-var lima = {
-  location: 'Lima',
-  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',],
-  minCustomersPerHour: 2,
-  maxCustomersPerHour: 16,
-  avgCookiesPerCustomer: 4.6,
-  dailySales: [],
-  dailyTotal: 0,
-  dayCalc() {
-    //1. for every hour of operation, 
-    for (var i = 0; i < this.hoursOfOperation.length; i++) {
-
-      //calculate a random number of customers... 
-      var customersPerHour = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour) + this.minCustomersPerHour);
-
-      //...and then using that a number of cookies sold
-      var cookiesSoldPerHour = Math.round(customersPerHour * this.avgCookiesPerCustomer);
-
-      //then push the data to the dailySales array
-      this.dailySales.push(`${this.hoursOfOperation[i]}: ${cookiesSoldPerHour} cookies`);
-
-      //update the running total of cookies sold
-      this.dailyTotal += cookiesSoldPerHour;
-    }
-
-    //then add the total to the end of the dailySales array
-    this.dailySales.push(`Total: ${this.dailyTotal} cookies`);
-  },
-  siteUpdate() {
-
-    //create a ul on sales.html for this store and name it
-    var salesList = document.createElement('ul');
-    salesList.textContent = `${this.location}`;
-
-    //for each hour of operation, create an li using info from the dailySales array, and append it to the salesList ul
-    for (var i = 0; i < this.dailySales.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = this.dailySales[i];
-      salesList.appendChild(li);
-    }
-
-    //then append the whole list to the site's master list
-    listsArea.appendChild(salesList);
-  }
-}
+var seattle = new Store('Seattle', 23, 65, 6.3);
+var tokyo = new Store('Tokyo', 3, 24, 1.2);
+var dubai = new Store('Dubai', 11, 38, 3.7);
+var paris = new Store('Paris', 20, 38, 2.3);
+var lima = new Store('Lima', 2, 16, 4.6);
 
 
 function updateAll() {
-  seattle.dayCalc();
-  seattle.siteUpdate();
-  tokyo.dayCalc();
-  tokyo.siteUpdate();
-  dubai.dayCalc();
-  dubai.siteUpdate();
-  paris.dayCalc();
-  paris.siteUpdate();
-  lima.dayCalc();
-  lima.siteUpdate();
+  for (var i = 0; i < Store.stores.length; i++) {
+    Store.stores[i].dayCalc();
+    Store.stores[i].siteUpdate();
+  }
 }
 
 updateAll();

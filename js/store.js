68b87299de7b3cb383dae275f22c8aa83cb61c78
Lucas Wilber <lucasjwilber@gmail.com>
2019-09-19 17:45:19 -0700

    almost finished the store order forms/script and the private pending orders page, realized that i need the data to be saved to storage somewhere in order for each page to communicate with each other. hid the page from the nav bar for now, and put the JS for it in a separate file.

--- js/store.js
+++ js/store.js
@@ -1,20 +1,62 @@
 'use strict';
 
 //purchase form stuff:
+
+var pendingOrdersList = document.getElementById('pending-orders');
 var purchaseForm = document.getElementById('purchaseForm');
 purchaseForm.addEventListener('submit', processOrder);
 var pendingOrders = [];
 var shippedOrders = [];
 
+function Order(cookies, cutters, shirts, customerName, customerStreet, customerCity, customerState, customerZip, customerCreditCard, customerCCV, specialInstructions) {
+  this.cookies = cookies;
+  this.cutters = cutters;
+  this.shirts = shirts;
+  this.customerName = customerName;
+  this.customerStreet = customerStreet;
+  this.customerCity = customerCity;
+  this.customerState = customerState;
+  this.customerZip = customerZip;
+  this.customerCreditCard = customerCreditCard;
+  this.customerCCV = customerCCV;
+  this.specialInstructions = specialInstructions;
+  pendingOrders.push(this);
+}
+
 function processOrder(event) {
   event.preventDefault();
+  var orderDetails = [];
   var cookies = event.target.buyCookiesField.value;
   var cutters = event.target.buyCuttersField.value;
-  var shirts = event.target.butShirtsField.value;
+  var shirts = event.target.buyShirtsField.value;
   var customerName = event.target.nameField.value;
-  console.log(cookies, cutters, shirts, customerName);
+  var customerStreet = event.target.streetField.value;
+  var customerCity = event.target.cityField.value;
+  var customerState = event.target.stateField.value;
+  var customerZip = event.target.zipField.value;
+  var customerCreditCard = event.target.creditCardField.value;
+  var customerCCV = event.target.ccvField.value;
+  var specialInstructions = event.target.specialInstructionsField;
 
+  new Order(cookies, cutters, shirts, customerName, customerStreet, customerCity, customerState, customerZip, customerCreditCard, customerCCV, specialInstructions);
 
+  console.log(pendingOrders);
+  alert('Order processed. Thank you for your purchase!');
 
+  event.target.buyCookiesField.value = null;
+  event.target.buyCuttersField.value = null;
+  event.target.buyShirtsField.value = null;
+  event.target.nameField.value = null;
+  event.target.streetField.value = null;
+  event.target.cityField.value = null;
+  event.target.stateField.value = null;
+  event.target.zipField.value = null;
+  event.target.creditCardField.value = null;
+  event.target.ccvField.value = null;
+  event.target.specialInstructionsField = null;
+
+  thisOrder = document.createElement('li')
+  pendingOrdersList.appendChild(thisOrder);
+  thisOrder.textContent = test;
 
 }
